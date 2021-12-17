edbab93bb28a4a06fd54773f031a71fd82263754
sheikhsadi9494 <sadis6343@gmail.com>
2021-12-17 14:25:23 +0600

    third commit

--- src/components/inventory/Inventory.js
+++ src/components/inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-            <h2>Inventory page</h2>
+            <h2>Inventory page !!</h2>
         </div>
     );
 };
